a39244d865e9d8d75ee0611e3689d8fec6a0a473
HoshiMika <juan_david_bayona@hotmail.com>
2025-11-11 18:19:53 -0500

    Prueba 13

--- JS/JS_Index/CuerpoPagina.js
+++ JS/JS_Index/CuerpoPagina.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   cont.innerHTML = `
     <section class="hero">
       <div class="container hero-content">
-        <img src="./Imagenes/Logo Nuevo.png" alt="Logo SOQUAL" class="hero-logo">
+        <img src="/Imagenes/Logo_Nuevo.png" alt="Logo SOQUAL" class="hero-logo">
         <div class="hero-text">
           <h2>Soporte Técnico Especializado en Tecnología</h2>
           <p>Soluciones integrales para computadores, aplicaciones, páginas web y sistemas de seguridad</p>
